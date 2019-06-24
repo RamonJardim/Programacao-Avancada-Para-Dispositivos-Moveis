f0dc06490d614c0e431a3a4d9abb9ca048c296c4
Ramon <ramon-neres@hotmail.com>
2019-06-23 21:15:09 -0300

    Adição do projeto MultiplePhotoPicker

---
+++ MultiplePhotoPicker/app/src/main/java/com/example/multiplephotopicker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.multiplephotopicker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun selectPhotosOnClick(view: View) {
+        intentPhotos = Intent()
+    }
+}
